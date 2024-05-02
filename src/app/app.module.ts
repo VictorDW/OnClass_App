c4b1d037871fe291c2ae67641dfb571d1f261e18
Victor Agudelo <viactur11@gmail.com>
2024-05-02 09:50:18 -0500

    Se creo un componente para las imagenes, y se actualizo el cambio en los correspondientes componentes padres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LinkComponent } from './atomic-design/atoms/link/link.component';
 import { NavComponent } from './atomic-design/molecules/nav/nav.component';
 import { HeaderComponent } from './atomic-design/molecules/header/header.component';
+import { ImageComponent } from './atomic-design/atoms/image/image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LinkComponent,
     NavComponent,
-    HeaderComponent
+    HeaderComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
