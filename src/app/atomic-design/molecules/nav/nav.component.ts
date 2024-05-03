a8c2cecfcc34fdfd5b797bed09395221564af7be
Victor Agudelo <viactur11@gmail.com>
2024-05-02 19:17:42 -0500

    Debido al cambio en el componente link, se actulizo la información que se le pasaba por medio del nav

--- src/app/atomic-design/molecules/nav/nav.component.ts
+++ src/app/atomic-design/molecules/nav/nav.component.ts
@@ -18,13 +18,17 @@ export class NavComponent {
     this.navItems = [
       {
         _href: '/home',
-        _title: 'Inicio',
-        _icon: 'fa-solid fa-house'
+        _link: {
+          _title: 'Inicio',
+          _icon: 'fa-solid fa-house'
+        }
       },
       {
-        _href: '#',
-        _title: 'Biblioteca',
-        _icon: 'fa-solid fa-book-open'
+        _href: '/library',
+        _link: {
+          _title: 'Biblioteca',
+          _icon: 'fa-solid fa-book-open'
+        }
       }
     ]
    }
