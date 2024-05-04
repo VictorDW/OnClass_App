1a4e11305f1de024317009a7e0296b1776daa936
Victor Agudelo <viactur11@gmail.com>
2024-05-03 19:37:24 -0500

    Se organizo la vista para cuando no hay ninguna tecnologia registrada

--- src/app/atomic-design/organisms/content-technology/content-technology.component.ts
+++ src/app/atomic-design/organisms/content-technology/content-technology.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { buttonStructure } from 'src/app/atomic-design/atoms/button/util/buttonStructure';
 
 @Component({
   selector: 'app-content-technology',
   templateUrl: './content-technology.component.html',
   styleUrls: ['./content-technology.component.scss']
 })
-export class ContentTechnologyComponent implements OnInit {
+
+export class ContentTechnologyComponent {
 
   private isShow = false;
+  private dataButton!: buttonStructure
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor() {
+    this.dataButton = {
+      _showIcon: true,
+      _icon: 'fa-solid fa-plus',
+      _text: 'Crear'
+      };
+    
+   }
 
   showContent(): boolean {
     return this.isShow;
+  }
+
+  itemButton(): buttonStructure {
+    return this.dataButton;
   }
 
 }
