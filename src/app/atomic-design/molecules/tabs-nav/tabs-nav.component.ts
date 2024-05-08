bba8b1754c549fe06da46569d5295fd35814d6a8
Victor Agudelo <viactur11@gmail.com>
2024-05-08 11:41:43 -0500

    se corrigieron los estilos del componente Tabs

--- src/app/atomic-design/molecules/tabs-nav/tabs-nav.component.ts
+++ src/app/atomic-design/molecules/tabs-nav/tabs-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { tabsStructure } from './util/TabsStructure';
 
 @Component({
@@ -6,7 +6,7 @@ import { tabsStructure } from './util/TabsStructure';
   templateUrl: './tabs-nav.component.html',
   styleUrls: ['./tabs-nav.component.scss']
 })
-export class TabsNavComponent implements OnInit {
+export class TabsNavComponent {
 
   tabs: tabsStructure[] = [];
 
@@ -14,23 +14,20 @@ export class TabsNavComponent implements OnInit {
     this.tabs =[{
       _href:'/library/technology',
       _link: {
-        _title: 'Technology'
-      } 
+        _title: 'Tecnologías'
+      }
     },{
       _href:'/library/capacity',
       _link: {
-        _title: 'Capacity'
+        _title: 'Capacidades'
       }
-      
+
     },{
       _href:'/bootcamp',
       _link: {
-        _title: 'Bootcamp'
-      } 
+        _title: 'Bootcamps'
+      }
     }];
    }
-
-  ngOnInit(): void {
-  }
 
 }
