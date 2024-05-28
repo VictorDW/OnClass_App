705b5e8b821de40c5b227cc94efcf3c333dead3e
Victor Agudelo <viactur11@gmail.com>
2024-05-27 21:27:21 -0500

    refactor-HU2[list-models.component]: refactorizo la implementación de la paginación, y se organizo el codigo para que fuera mas legible, además se le agregaron los estilos definidos

--- src/app/atomic-design/organisms/list-models/list-models.component.ts
+++ src/app/atomic-design/organisms/list-models/list-models.component.ts
@@ -16,7 +16,8 @@ type ButtonDirection = {
 
 type Pages = {
   contents:  number | string ,
-  value: number
+  value: number,
+  className?: string
 }
 
 @Component({
@@ -38,9 +39,12 @@ export class ListModelsComponent {
   models: Technology[] = [];
   KeyEnum = KeyEnum;
 
-  pageNumbers: Pages[] = [];
+  pages: Pages[] = [];
   currentPage = 1;
-  pageAmount!: number;
+  totalPages!: number;
+  firstPage = true;
+  lastPage = false;
+
   constants = {
     ONE_VALUE: 1,
     TWO_VALUE: 2,
@@ -49,8 +53,8 @@ export class ListModelsComponent {
     DOTS_KEY: '...',
     PAGINATION_NUMBER_CLASS: 'paginationNumber',
     PAGINATION_DOTS_CLASS: 'paginationDots',
-    ACTIVE_CLASS: 'active',
-    EMPTY_STRING: ''
+    ACTIVE_CLASS: 'page--active',
+    EMPTY_STRING: '',
   };
 
   constructor(private getService: GetService, private _serviceListModel: ListModelService) {
@@ -64,96 +68,125 @@ export class ListModelsComponent {
   }
 
 
-   getPageNumbers(): Pages[] {
 
-    this.pageNumbers = [];
+  private createPagination(): Pages[] {
 
-    if(this.currentPage === this.constants.ONE_VALUE && this.pageAmount == this.constants.ONE_VALUE) {
-      this.pageNumbers.push({
-        contents: String(this.constants.ONE_VALUE), 
-        value: this.constants.ONE_VALUE
-      });
+    this.pages = [];
+
+    if(this.isSinglePage()) {
+      this.addSinglePage();
     }
 
-    if(this.currentPage < this.pageAmount && this.currentPage !== this.pageAmount) { //LLena el array con tres numeros, sin superar la cantidad total de paginas ni la pagina actual + dos posiciones mas
-      
-      for(let index = this.currentPage; (index <= this.pageAmount) && (index <= (this.currentPage + this.constants.TWO_VALUE) ); index++) {
-        
-        this.pageNumbers.push({
-          contents: String(index), 
-          value: index
-        });
-      }
-
-      
+    if(this.shouldAddConsecutivePages()) {
+      this.addConsecutivePages(); 
     }
 
-   if(this.pageAmount >= this.constants.TWO_VALUE) {//Solo aplica cuando hay mas de do paginas
+    if(this.hasMultiplePages()) {
 
-      if(this.currentPage === this.pageAmount) {//LLena el array con tres numeros, pero esta vez desde la ultima pagina hasta la penultima
+      this.addPagesForLastPage();
+      this.addPenultimePage();
+      this.addDotsAndLastPage();
+      this.addFirstPageAndDots();
+      this.addLastPage();
+    }
+
+    console.log(this.currentPage);
+    return this.pages;
+  }
+
+  private createDataPage(text: string | number, value: number, className?: string): Pages {
+    return {
+      contents: text,
+      value: value,
+      className: className
+    };
+  }
+
+
+  private isSinglePage(): boolean {
+    return this.currentPage === this.constants.ONE_VALUE && this.totalPages == this.constants.ONE_VALUE;
+  }
+
+  private shouldAddConsecutivePages(): boolean {
+    //LLena el array con tres numeros, sin superar la cantidad total de paginas ni la pagina actual + dos paginas
+    return this.currentPage < this.totalPages;
+  }
+
+  private hasMultiplePages(): boolean {
+    return this.totalPages >= this.constants.TWO_VALUE;
+  }
+
+  private addSinglePage(): void {
+    this.pages.push(
+      this.createDataPage(this.constants.ONE_VALUE, this.constants.ONE_VALUE, this.constants.ACTIVE_CLASS)
+    );
+  }
+
+  private addConsecutivePages(): void {
+    for (let index = this.currentPage; (index <= this.totalPages) && (index <= (this.currentPage + this.constants.TWO_VALUE)); index++) {
+      this.pages.push(this.createDataPage(index, index, (index === this.currentPage) ? this.constants.ACTIVE_CLASS : ''));
+    }
+  }
+
+  private addPagesForLastPage(): void {
+
+    //LLena el array con tres numeros, pero esta vez desde la ultima pagina hasta la ante penultima
+    if(this.currentPage === this.totalPages) {
         
-        for(let index = this.currentPage; index >= (this.pageAmount - this.constants.TWO_VALUE); index--) {
-          if(index >= this.constants.ONE_VALUE) {
-            this.pageNumbers.unshift({
-              contents: String(index), 
-              value: index, 
-            });
-          }
-        }
+      const beforePenultimatePage = this.totalPages - this.constants.TWO_VALUE;
+
+      for(let index = this.currentPage; (index >= beforePenultimatePage) && (index >= this.constants.ONE_VALUE); index--) {
+          this.pages.unshift(this.createDataPage(index, index, (index === this.currentPage) ? this.constants.ACTIVE_CLASS : ''));
       }
+    }
+  }
 
-      if(this.currentPage === (this.pageAmount - this.constants.ONE_VALUE) && (this.pageAmount - this.constants.TWO_VALUE) >= this.constants.ONE_VALUE) { //Agrega la penultima posición, si se esta en la ante penultima pagina, ejemplo -> 9 -> array[8,9,10]
-        
-        const penulPosition = this.pageAmount - this.constants.TWO_VALUE
-        
-          this.pageNumbers.unshift({
-            contents: String(penulPosition), 
-            value: penulPosition,
-          });
-      }
+  private addPenultimePage(): void {
 
-      //agrega un string "..." y la ultima posición, si la suma entre la pagina actual y tres posciones mas siguen siendo menor al total de paginas
-      if((this.currentPage + this.constants.THREE_VALUE) < this.pageAmount) { 
+    const penultimatePosition = this.totalPages - this.constants.ONE_VALUE;
+    const beforePenultimatePosition = this.totalPages - this.constants.TWO_VALUE;
 
-        this.pageNumbers.push({
-          contents: this.constants.DOTS_KEY,
-          value: this.currentPage
-        },{
-          contents: String(this.pageAmount),
-          value: this.pageAmount,
-        });
-      }
+    //Agrega la ante penultima pagina, si se esta en la penultima pagina y que la ante penultima es mayor igual a 1, ejemplo -> 9 -> array[8,9,10]
+    if(this.currentPage === penultimatePosition  && beforePenultimatePosition >= this.constants.ONE_VALUE) { 
 
-      //agrega la primera posición y  un string "...", cuando el tolal de paginas es mayor o igual a 4, y ademas la pagina actual es mayor o igual a la penultima pagina
-      if(this.pageAmount >= this.constants.FOUR_VALUE && this.currentPage >= (this.pageAmount - this.constants.TWO_VALUE)) {
+        this.pages.unshift(this.createDataPage(beforePenultimatePosition, beforePenultimatePosition));
+    }
+  }
 
-        this.pageNumbers.unshift({
-          contents: this.constants.ONE_VALUE,
-          value: this.constants.ONE_VALUE
-        }, {
-          contents: this.constants.DOTS_KEY,
-          value: this.currentPage
-        })
-      }
+  private addDotsAndLastPage(): void {
+    //agrega un string "..." y la ultima posición, si la suma entre la pagina actual y tres posciones mas siguen siendo menor al total de paginas
+    if((this.currentPage + this.constants.THREE_VALUE) < this.totalPages) { 
 
-      //agrega la ultima pagina, cuando la pagina actual + tres posiciones mas es igual al total de paginas
-      if(this.currentPage + this.constants.THREE_VALUE === this.pageAmount) {
+      this.pages.push(
+        this.createDataPage(this.constants.DOTS_KEY, this.currentPage),
+        this.createDataPage(String(this.totalPages), this.totalPages)
+      );
+    }
+  }
 
-        this.pageNumbers.push({
-          contents: String(this.pageAmount),
-          value: this.pageAmount,
-        });
-      }
+  private addFirstPageAndDots(): void {
 
+    const beforePenultimatePosition = this.totalPages - this.constants.TWO_VALUE;
     
-    }
+    if(this.totalPages >= this.constants.FOUR_VALUE && this.currentPage >= beforePenultimatePosition) {
 
-    return this.pageNumbers;
+      this.pages.unshift(
+        this.createDataPage(this.constants.ONE_VALUE,this.constants.ONE_VALUE), 
+        this.createDataPage(this.constants.DOTS_KEY, this.currentPage)
+      );
+    }
+  }
+
+  private addLastPage(): void {
+    if(this.currentPage + this.constants.THREE_VALUE === this.totalPages) {
+
+      this.pages.push(this.createDataPage(this.totalPages, this.totalPages));
+    }
   }
 
   paginate(value: number): void {
     this.currentPage = value;
-    this.pageNumbers = this.getPageNumbers();
+    this.pages = this.createPagination();
     this.updatePage(this.currentPage - 1);
   }
 
@@ -186,21 +219,25 @@ export class ListModelsComponent {
     this.updateDirection(this.buttonDirection.text);
   }
 
- private fillObjectPagination(): void {
-    this._paginationDate = {
-      size: this.size,
-      direction: this.direction,
-      page: (this.currentPage - 1)
-    }
- }
+  private fillObjectPagination(): void {
+      this._paginationDate = {
+        size: this.size,
+        direction: this.direction,
+        page: (this.currentPage - 1)
+      }
+  }
 
   private populateModelList(): void {
+
     this._serviceListModel.modelObservable$.subscribe((data) => {
+
       this.displayContentStatus(!data.empty);
       this.models = data.content;
       this.currentPage = data.pageNumber + 1;
-      this.pageAmount = data.totalPages;
-      this.pageNumbers = this.getPageNumbers();
+      this.totalPages = data.totalPages;
+      this.firstPage = data.first;
+      this.lastPage = data.last;
+      this.pages = this.createPagination();
     });
   }
 
@@ -215,7 +252,6 @@ export class ListModelsComponent {
   updateSize(value: number): void {
     this._paginationDate.size = value;
     this._paginationDate.page = 0;
-    console.log(this._paginationDate)
     this.updateList(this._paginationDate);
   }
 
@@ -226,9 +262,6 @@ export class ListModelsComponent {
 
   updatePage(value: number): void {
     this._paginationDate.page = value;
-    console.log(this._paginationDate)
     this.updateList(this._paginationDate);
   }
-
-
 }
