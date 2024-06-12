d960376786b502e3dfa242b60e424e8275360d41
Victor Agudelo <viactur11@gmail.com>
2024-06-12 00:00:45 -0500

    feature-HU10[packege-lock.json, package.json, auth.service.ts]: se installo la libreria para decodificar el token, y se creo el servicio que maneja la logica para validar la autenticación y autorización

---
+++ src/app/shared/service/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  private getDecodedToken(): any {
+
+    const token = this.getToken();
+    return token ? jwtDecode(token) : null;
+  }
+
+  public getUserRole(): string | null {
+
+    const payload = this.getDecodedToken();
+    return payload ? payload.role : null;
+  }
+
+  public isAuthenticated(): boolean {
+    return this.getToken() !== null;
+  }
+
+  public hasRole(role: string[]): boolean {
+
+    if(!this.isAuthenticated() || role.length === 0) {
+      return false;
+    }
+
+    const userRole = this.getUserRole();
+    return userRole ? role.includes(userRole) : false;
+  }
+
+
+}
