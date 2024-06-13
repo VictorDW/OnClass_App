89f09964e89b075a39b877f92c86632050d8cfb3
Victor Agudelo <viactur11@gmail.com>
2024-06-12 23:18:14 -0500

    refactor-HU10[handler-error.service]: se refactorizo la implementación para manejar el mensaje de error y el lanzamiento de la alerta

--- src/app/shared/service/handler/handler-error.service.ts
+++ src/app/shared/service/handler/handler-error.service.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ResponseErrorMesages } from '../../constants/constants';
+import { MESSAGES_ALERT, ResponseErrorMesages } from '../../constants/constants';
+import { AlertService } from '../observables/alert.service';
 
 
 @Injectable({
@@ -9,10 +10,22 @@ import { ResponseErrorMesages } from '../../constants/constants';
 
 export class HandlerErrorService {
 
+  constructor(private _alertService: AlertService) {}
+
   handler(error: HttpErrorResponse) {
-    
-   return error.statusText === "Unknown Error" || error.status === 500? 
-      'Ha ocurrido un error inesperado' : 
+
+    if (error.status !== 401) {
+
+      const message = error.statusText === "Unknown Error" || error.status === 500?
+      MESSAGES_ALERT.DEFAULT :
       error.error.message
+
+      this.throwAlert(message);
+    }
   }
+
+  throwAlert(error: string) {
+    this._alertService.showAlert(error);
+  }
+
 }
